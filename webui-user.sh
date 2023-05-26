a9f494e926bbc2fdec70664196da0e0ea46598a3
Mike Stamm <mike@heatlossgames.com>
2023-05-25 21:15:34 -0400

    Local settings.

--- webui-user.sh
+++ webui-user.sh
@@ -10,7 +10,7 @@
 #clone_dir="stable-diffusion-webui"
 
 # Commandline arguments for webui.py, for example: export COMMANDLINE_ARGS="--medvram --opt-split-attention"
-#export COMMANDLINE_ARGS=""
+export COMMANDLINE_ARGS="--skip-torch-cuda-test --no-half --use-cpu interrogate"
 
 # python3 executable
 #python_cmd="python3"
@@ -25,7 +25,7 @@
 #export LAUNCH_SCRIPT="launch.py"
 
 # install command for torch
-#export TORCH_COMMAND="pip install torch==1.12.1+cu113 --extra-index-url https://download.pytorch.org/whl/cu113"
+export TORCH_COMMAND="pip install torch==1.12.1 torchvision==0.13.1 psutil"
 
 # Requirements file to use for stable-diffusion-webui
 #export REQS_FILE="requirements_versions.txt"
@@ -33,17 +33,19 @@
 # Fixed git repos
 #export K_DIFFUSION_PACKAGE=""
 #export GFPGAN_PACKAGE=""
+export K_DIFFUSION_REPO="https://github.com/brkirch/k-diffusion.git"
 
 # Fixed git commits
 #export STABLE_DIFFUSION_COMMIT_HASH=""
 #export TAMING_TRANSFORMERS_COMMIT_HASH=""
 #export CODEFORMER_COMMIT_HASH=""
 #export BLIP_COMMIT_HASH=""
+export K_DIFFUSION_COMMIT_HASH="51c9778f269cedb55a4d88c79c0246d35bdadb71"
 
 # Uncomment to enable accelerated launch
 #export ACCELERATE="True"
 
-# Uncomment to disable TCMalloc
-#export NO_TCMALLOC="True"
+# Enable CPU fallback for missing MPS ops
+export PYTORCH_ENABLE_MPS_FALLBACK=1
 
 ###########################################
